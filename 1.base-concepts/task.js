24c48c9d95b1a1b2cc0c4b729f60258a9fa88b9b
Elsukova Eugeniia <elsukova.eugeniia@gmail.com>
2025-11-08 17:27:53 +0300

    Первое задание полностью готово

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,31 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+
+	const d = b ** 2 - 4 * a * c;
+
+	if (d < 0) {
+		return arr;
+	}
+
+	if (d === 0) {
+		const root = -b / (2 * a);
+		arr.push(root);
+		return arr;
+	}
+
+	if (d > 0) {
+		const sqrtD = Math.sqrt(d);
+		const root1 = (-b + sqrtD) / (2 * a);
+		const root2 = (-b - sqrtD) / (2 * a);
+		arr.push(root1, root2);
+		return arr;
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
 }
