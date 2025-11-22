32ceeaeb3d8f7207031f01b00a5f9b369f04dfc3
Elsukova Eugeniia <elsukova.eugeniia@gmail.com>
2025-11-22 21:43:58 +0300

    Четвёртое задание полностью готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -3,14 +3,13 @@ function Student(name, gender, age) {
 	this.gender = gender;
 	this.age = age;
 	this.marks = [];
-
 }
 
 Student.prototype.setSubject = function(subjectName) {
 	this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function(...marksToAdd) {
+Student.prototype.addMarks = function(...marks) {
 
 	if (this.excluded) {
 		return;
@@ -21,23 +20,33 @@ Student.prototype.addMarks = function(...marksToAdd) {
 		return;
 	}
 
-	if (marksToAdd.length === 0) {
+
+	if (marks.length === 0) {
 		return;
 	}
 
-	this.marks.push(...marksToAdd);
+
+	this.marks.push(...marks);
 }
 
 Student.prototype.getAverage = function() {
+
 	if (!this.marks || this.marks.length === 0) {
 		return 0;
 	}
+
+
 	const sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+
+
 	return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function(reason) {
+
 	delete this.subject;
 	delete this.marks;
+
+
 	this.excluded = reason;
 }
