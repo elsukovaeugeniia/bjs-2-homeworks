72220821db821abbf329834afe8aaa15f83e64d2
Elsukova Eugeniia <elsukova.eugeniia@gmail.com>
2025-11-25 19:56:36 +0300

    Шестое задание полностью готово

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -18,44 +18,44 @@ function validateCount(value) {
 
 
 class Triangle {
-  constructor(a, b, c) {
-    if (a + b <= c || a + c <= b || b + c <= a) {
-      throw new Error("Треугольник с такими сторонами не существует");
-    }
+	constructor(a, b, c) {
+		if (a + b <= c || a + c <= b || b + c <= a) {
+			throw new Error("Треугольник с такими сторонами не существует");
+		}
 
-    this.a = a;
-    this.b = b;
-    this.c = c;
-  }
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
 
-  get perimeter() {
-    return this.a + this.b + this.c;
-  }
+	get perimeter() {
+		return this.a + this.b + this.c;
+	}
 
-  get area() {
-    const p = this.perimeter / 2;
-    const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
-    return parseFloat(area.toFixed(3));
-  }
+	get area() {
+		const p = this.perimeter / 2;
+		const area = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+		return parseFloat(area.toFixed(3));
+	}
 }
 
 function getTriangle(a, b, c) {
-  try {
-    return new Triangle(a, b, c);
-  } catch (error) {
-    
-    return Object.defineProperty({}, 'perimeter', {
-      get() {
-        return "Ошибка! Треугольник не существует";
-      },
-      configurable: false,
-      enumerable: true
-    }).defineProperty('area', {
-      get() {
-        return "Ошибка! Треугольник не существует";
-      },
-      configurable: false,
-      enumerable: true
-    });
-  }
+	try {
+		return new Triangle(a, b, c);
+	} catch (error) {
+
+		return Object.defineProperty({}, 'perimeter', {
+			get() {
+				return "Ошибка! Треугольник не существует";
+			},
+			configurable: false,
+			enumerable: true
+		}).defineProperty('area', {
+			get() {
+				return "Ошибка! Треугольник не существует";
+			},
+			configurable: false,
+			enumerable: true
+		});
+	}
 }
